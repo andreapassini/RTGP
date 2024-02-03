f9c6083bdcf8d67a535077b2829da9c07e65bd76
Andrea Passini <andreapa98@gmail.com>
2024-02-03 23:40:14 +0100

    update config

--- RTGP_LAB/project/project01/MakefileWin.bat
+++ RTGP_LAB/project/project01/MakefileWin.bat
@@ -1,8 +1,8 @@
 @echo off
-IF EXIST "D:\Programmi\Microsoft Visual Studio\BuildTools2022\VC\Auxiliary\Build\vcvarsall.bat" (
-    call "D:\Programmi\Microsoft Visual Studio\BuildTools2022\VC\Auxiliary\Build\vcvarsall.bat" x64
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 ) ELSE (
-    call "D:\Programmi\Microsoft Visual Studio\BuildTools2022\VC\Auxiliary\Build\vcvarsall.bat" x64
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 )
 
 if [%1%]==[] (
@@ -10,5 +10,3 @@ if [%1%]==[] (
 ) else (
   nmake /f MakefileWin clean
 )
-
-
